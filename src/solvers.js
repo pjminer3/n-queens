c10cb9f60641fa037088f3431e75476076398b54
pjminer3 <patrickminer318@gmail.com>
2017-10-19 11:53:22 -0700

    Completed findNRooksSolution

--- src/solvers.js
+++ src/solvers.js
@@ -16,7 +16,40 @@
 
 
 window.findNRooksSolution = function(n) {
-  var solution = undefined; //fixme
+  // creates base blank board object
+  var realBoard = new Board({n: n});
+
+  // internal recursive function to add rooks to board
+  function addRook(board, numRooks, usedRows, usedCols) {
+    // creates board-array representation to view pieces, along with setting used rows/columns if none are passed in
+    var boardRows = board.rows()
+    var usedRows = usedRows || [];
+    var usedCols = usedCols || [];
+
+    // base case: if we don't need to add any more rooks, return out of recursion
+    if (numRooks === 0) { return; }
+
+    // loop through all rows; column index = i
+    for (var i = 0; i < boardRows.length; i++) {
+      // loop through all columns; column index = j
+      for (var j = 0; j < boardRows.length; j++) {
+        // check to see if the pointed to space is available, if so, add rook to it and call again with 1 less rook to place
+        if (boardRows[i][j] === 0 && !usedRows.includes(i) && !usedCols.includes(j)) {
+          board.togglePiece(i, j);
+          usedRows.push(i);
+          usedCols.push(j);
+          return addRook(board, numRooks - 1, usedRows, usedCols);
+        }
+      }
+    }
+
+    return;
+  }
+
+  addRook(realBoard, n);
+  
+  console.log(realBoard.rows());
+  var solution = realBoard.rows(); //fixme
 
   console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
   return solution;
